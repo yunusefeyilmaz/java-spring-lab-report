1a98cbfa0407d1775ed14abd942a9692b47e91c7
Yunus Efe Yılmaz <yunusefeyilmaz@outlook.com>
2024-04-30 15:34:42 +0300

    init project

---
+++ src/main/java/yunusefeyilmaz/laboratoryreport/LaboratoryreportApplication.java
@@ -0,0 +1,13 @@
+package yunusefeyilmaz.laboratoryreport;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LaboratoryreportApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LaboratoryreportApplication.class, args);
+	}
+
+}
---
+++ src/test/java/yunusefeyilmaz/laboratoryreport/LaboratoryreportApplicationTests.java
@@ -0,0 +1,13 @@
+package yunusefeyilmaz.laboratoryreport;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LaboratoryreportApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
