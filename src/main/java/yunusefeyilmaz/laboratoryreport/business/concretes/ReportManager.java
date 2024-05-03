ae6c4a59bf5f7c8f310a34af322db778051f5810
Yunus Efe Yılmaz <yunusefeyilmaz@outlook.com>
2024-05-03 16:10:33 +0300

    Add Report Service

---
+++ src/main/java/yunusefeyilmaz/laboratoryreport/business/concretes/ReportManager.java
@@ -0,0 +1,12 @@
+package yunusefeyilmaz.laboratoryreport.business.concretes;
+
+import org.springframework.stereotype.Service;
+
+import lombok.AllArgsConstructor;
+import yunusefeyilmaz.laboratoryreport.business.abstracts.ReportService;
+
+@Service
+@AllArgsConstructor
+public class ReportManager implements ReportService {
+
+}
