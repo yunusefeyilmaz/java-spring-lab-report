61769686714f015df03a0d7e393e03a43dbd103c
Yunus Efe Yılmaz <yunusefeyilmaz@outlook.com>
2024-05-03 16:10:10 +0300

    Add LabAssistant Service

---
+++ src/main/java/yunusefeyilmaz/laboratoryreport/business/concretes/LabAssistantManager.java
@@ -0,0 +1,12 @@
+package yunusefeyilmaz.laboratoryreport.business.concretes;
+
+import org.springframework.stereotype.Service;
+
+import lombok.AllArgsConstructor;
+import yunusefeyilmaz.laboratoryreport.business.abstracts.LabAssistantService;
+
+@Service
+@AllArgsConstructor
+public class LabAssistantManager implements LabAssistantService {
+
+}
