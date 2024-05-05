b383cc00e55a2be3bee13d7164fb0139f478ca6d
Yunus Efe Yılmaz <yunusefeyilmaz@outlook.com>
2024-05-05 15:26:42 +0300

    Created LabAssistant interface methods

--- src/main/java/yunusefeyilmaz/laboratoryreport/business/abstracts/LabAssistantService.java
+++ src/main/java/yunusefeyilmaz/laboratoryreport/business/abstracts/LabAssistantService.java
@@ -1,5 +1,14 @@
 package yunusefeyilmaz.laboratoryreport.business.abstracts;
 
-public interface LabAssistantService {
+import java.util.List;
 
+import yunusefeyilmaz.laboratoryreport.business.requests.CreateLabAssistantRequest;
+import yunusefeyilmaz.laboratoryreport.business.requests.UpdateLabAssistantRequest;
+import yunusefeyilmaz.laboratoryreport.business.response.GetAllLabAssistantsResponse;
+
+public interface LabAssistantService {
+	List<GetAllLabAssistantsResponse> getAll();
+	void add(CreateLabAssistantRequest createLabAssistantRequest);
+	void update(UpdateLabAssistantRequest updateLabAssistantRequest);
+	void delete(int id);
 }
