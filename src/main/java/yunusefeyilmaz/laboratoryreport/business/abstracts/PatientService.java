5290b50724e1567ab355d6bb7ccada049f04c0ae
Yunus Efe Yılmaz <yunusefeyilmaz@outlook.com>
2024-05-06 16:07:02 +0300

    Created Patient interface methods

--- src/main/java/yunusefeyilmaz/laboratoryreport/business/abstracts/PatientService.java
+++ src/main/java/yunusefeyilmaz/laboratoryreport/business/abstracts/PatientService.java
@@ -1,5 +1,14 @@
 package yunusefeyilmaz.laboratoryreport.business.abstracts;
 
-public interface PatientService {
+import java.util.List;
 
+import yunusefeyilmaz.laboratoryreport.business.requests.CreatePatientRequest;
+import yunusefeyilmaz.laboratoryreport.business.requests.UpdatePatientRequest;
+import yunusefeyilmaz.laboratoryreport.business.response.GetAllPatientsResponse;
+
+public interface PatientService {
+	List<GetAllPatientsResponse> getAll();
+	void add(CreatePatientRequest createPatientRequest);
+	void update(UpdatePatientRequest updatePatientRequest);
+	void delete(int id);
 }
