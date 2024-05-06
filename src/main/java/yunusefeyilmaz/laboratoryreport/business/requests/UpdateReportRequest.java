ba9fcc08a46eb59e555406acfb46e29b627c54d3
Yunus Efe Yılmaz <yunusefeyilmaz@outlook.com>
2024-05-06 16:08:35 +0300

    Add Report Request and Response Data

---
+++ src/main/java/yunusefeyilmaz/laboratoryreport/business/requests/UpdateReportRequest.java
@@ -0,0 +1,23 @@
+package yunusefeyilmaz.laboratoryreport.business.requests;
+
+import java.util.Date;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import yunusefeyilmaz.laboratoryreport.entities.LabAssistant;
+import yunusefeyilmaz.laboratoryreport.entities.Patient;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class UpdateReportRequest {
+	private int id;
+	private Patient patient;
+	private LabAssistant labAssistant;
+    private String fileNumber;
+    private String diagnosisTitle;
+    private String diagnosisDetails;
+    private Date reportDate;
+    private String imagePath;
+}
