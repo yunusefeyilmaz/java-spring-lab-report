f0c121f8d85c4f3e654281ace3997b13a0603b87
Yunus Efe Yılmaz <yunusefeyilmaz@outlook.com>
2024-05-06 16:09:15 +0300

    Created Report interface methods

--- src/main/java/yunusefeyilmaz/laboratoryreport/business/abstracts/ReportService.java
+++ src/main/java/yunusefeyilmaz/laboratoryreport/business/abstracts/ReportService.java
@@ -1,5 +1,14 @@
 package yunusefeyilmaz.laboratoryreport.business.abstracts;
 
-public interface ReportService {
+import java.util.List;
 
+import yunusefeyilmaz.laboratoryreport.business.requests.CreateReportRequest;
+import yunusefeyilmaz.laboratoryreport.business.requests.UpdateReportRequest;
+import yunusefeyilmaz.laboratoryreport.business.response.GetAllReportsResponse;
+
+public interface ReportService {
+	List<GetAllReportsResponse> getAll();
+	void add(CreateReportRequest createReportRequest);
+	void update(UpdateReportRequest updateReportRequest);
+	void delete(int id);
 }
