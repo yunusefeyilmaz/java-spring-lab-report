66db9619444ba5b259da710a9a4a00aa9cefb1f4
Yunus Efe Yılmaz <yunusefeyilmaz@outlook.com>
2024-05-09 16:11:49 +0300

    Add Model Mapper

--- src/main/java/yunusefeyilmaz/laboratoryreport/LaboratoryreportApplication.java
+++ src/main/java/yunusefeyilmaz/laboratoryreport/LaboratoryreportApplication.java
@@ -1,13 +1,26 @@
 package yunusefeyilmaz.laboratoryreport;
 
+import org.modelmapper.ModelMapper;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+import io.github.cdimascio.dotenv.Dotenv;
+
 
 @SpringBootApplication
 public class LaboratoryreportApplication {
 
 	public static void main(String[] args) {
+		Dotenv dotenv = Dotenv.load();
+		System.setProperty("DB_URL", dotenv.get("DB_URL"));
+        System.setProperty("DB_USERNAME", dotenv.get("DB_USERNAME"));
+        System.setProperty("DB_PASSWORD", dotenv.get("DB_PASSWORD"));
 		SpringApplication.run(LaboratoryreportApplication.class, args);
 	}
-
+	
+	@Bean
+	public ModelMapper getModelMapper() {
+		return new ModelMapper();
+	}
 }
