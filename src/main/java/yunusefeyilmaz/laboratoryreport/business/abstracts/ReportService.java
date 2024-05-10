9cd3c81919367a61bc733b64a40a522c22022615
Yunus Efe Yılmaz <yunusefeyilmaz@outlook.com>
2024-05-10 16:32:39 +0300

    Add getReport Method

--- src/main/java/yunusefeyilmaz/laboratoryreport/business/abstracts/ReportService.java
+++ src/main/java/yunusefeyilmaz/laboratoryreport/business/abstracts/ReportService.java
@@ -5,10 +5,12 @@ import java.util.List;
 import yunusefeyilmaz.laboratoryreport.business.requests.CreateReportRequest;
 import yunusefeyilmaz.laboratoryreport.business.requests.UpdateReportRequest;
 import yunusefeyilmaz.laboratoryreport.business.response.GetAllReportsResponse;
+import yunusefeyilmaz.laboratoryreport.business.response.GetReportResponse;
 
 public interface ReportService {
 	List<GetAllReportsResponse> getAll();
 	void add(CreateReportRequest createReportRequest);
 	void update(UpdateReportRequest updateReportRequest);
-	void delete(int id);
+	void delete(Long id);
+	GetReportResponse getReport(Long id);
 }
---
+++ src/main/java/yunusefeyilmaz/laboratoryreport/business/response/GetReportResponse.java
@@ -0,0 +1,23 @@
+package yunusefeyilmaz.laboratoryreport.business.response;
+
+import java.util.Date;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import yunusefeyilmaz.laboratoryreport.entities.LabAssistant;
+import yunusefeyilmaz.laboratoryreport.entities.Patient;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class GetReportResponse {
+	private Long id;
+	private Patient patient;
+	private LabAssistant labAssistant;
+    private String fileNumber;
+    private String diagnosisTitle;
+    private String diagnosisDetails;
+    private Date reportDate;
+    private byte[] image;
+}
