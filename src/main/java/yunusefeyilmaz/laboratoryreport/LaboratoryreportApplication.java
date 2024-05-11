e80ec2f183ad0c525580e5ec4e47beb65f3397be
Yunus Efe Yılmaz <yunusefeyilmaz@outlook.com>
2024-05-11 21:23:46 +0300

    Add database connection

--- src/main/java/yunusefeyilmaz/laboratoryreport/LaboratoryreportApplication.java
+++ src/main/java/yunusefeyilmaz/laboratoryreport/LaboratoryreportApplication.java
@@ -4,11 +4,13 @@ import org.modelmapper.ModelMapper;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 import io.github.cdimascio.dotenv.Dotenv;
 
 
 @SpringBootApplication
+@RestControllerAdvice
 public class LaboratoryreportApplication {
 
 	public static void main(String[] args) {
