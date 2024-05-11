e87b1f84a93fdc0be93cac1228837217a7930c65
Yunus Efe Yılmaz <yunusefeyilmaz@outlook.com>
2024-05-11 21:18:11 +0300

    Add custom search method on repository interfaces

--- src/main/java/yunusefeyilmaz/laboratoryreport/dataAccess/abstracts/PatientRepository.java
+++ src/main/java/yunusefeyilmaz/laboratoryreport/dataAccess/abstracts/PatientRepository.java
@@ -1,11 +1,13 @@
 package yunusefeyilmaz.laboratoryreport.dataAccess.abstracts;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import yunusefeyilmaz.laboratoryreport.entities.Patient;
 
 @Repository
-public interface PatientRepository extends JpaRepository<Patient, Integer> {
-
+public interface PatientRepository extends JpaRepository<Patient, Long> {
+	 List<Patient> findByNameContainingIgnoreCaseOrSurnameContainingIgnoreCase(String name, String surname);
 }
--- src/main/java/yunusefeyilmaz/laboratoryreport/dataAccess/abstracts/ReportRepository.java
+++ src/main/java/yunusefeyilmaz/laboratoryreport/dataAccess/abstracts/ReportRepository.java
@@ -1,11 +1,14 @@
 package yunusefeyilmaz.laboratoryreport.dataAccess.abstracts;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import yunusefeyilmaz.laboratoryreport.entities.Report;
 
 @Repository
-public interface ReportRepository extends JpaRepository<Report, Integer> {
-
+public interface ReportRepository extends JpaRepository<Report, Long> {
+	List<Report> findByPatient_Id(Long patientId);
+    List<Report> findAllByOrderByReportDateDesc();
 }
