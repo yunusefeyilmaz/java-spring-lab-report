c05cc2bfef6336024a5ee0386cb488541e8549c4
Yunus Efe Yılmaz <yunusefeyilmaz@outlook.com>
2024-05-17 16:01:23 +0300

    Add findByPatientID methods

--- src/main/java/yunusefeyilmaz/laboratoryreport/business/abstracts/PatientService.java
+++ src/main/java/yunusefeyilmaz/laboratoryreport/business/abstracts/PatientService.java
@@ -6,11 +6,14 @@ import yunusefeyilmaz.laboratoryreport.business.requests.CreatePatientRequest;
 import yunusefeyilmaz.laboratoryreport.business.requests.UpdatePatientRequest;
 import yunusefeyilmaz.laboratoryreport.business.response.GetAllPatientsResponse;
 import yunusefeyilmaz.laboratoryreport.business.response.GetPatientResponse;
+import yunusefeyilmaz.laboratoryreport.entities.Patient;
 
 public interface PatientService {
 	List<GetAllPatientsResponse> getAll();
-	void add(CreatePatientRequest createPatientRequest);
+	Patient add(CreatePatientRequest createPatientRequest);
 	void update(UpdatePatientRequest updatePatientRequest);
 	void delete(Long id);
 	GetPatientResponse getPatient(Long id);
+	GetPatientResponse findOneByPatientId(String patientID);
+	Patient findOrCreatePatient(Patient patient);
 }
