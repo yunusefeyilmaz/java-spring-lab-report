bce7d87a04f024459bfe2982ada0527efe93f62e
Yunus Efe Yılmaz <yunusefeyilmaz@outlook.com>
2024-05-18 19:01:26 +0300

    Add Report Controller Methods

--- src/main/java/yunusefeyilmaz/laboratoryreport/webApi/controllers/ReportController.java
+++ src/main/java/yunusefeyilmaz/laboratoryreport/webApi/controllers/ReportController.java
@@ -1,5 +1,59 @@
 package yunusefeyilmaz.laboratoryreport.webApi.controllers;
 
-public class ReportController {
+import org.springframework.web.bind.annotation.RestController;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+import lombok.AllArgsConstructor;
+import yunusefeyilmaz.laboratoryreport.business.abstracts.ReportService;
+import yunusefeyilmaz.laboratoryreport.business.requests.CreateReportRequest;
+import yunusefeyilmaz.laboratoryreport.business.requests.UpdateReportRequest;
+import yunusefeyilmaz.laboratoryreport.business.response.GetAllReportsResponse;
+import yunusefeyilmaz.laboratoryreport.business.response.GetReportResponse;
+
+@RestController
+@RequestMapping("/api/reports")
+@AllArgsConstructor
+public class ReportController {
+	
+	@Autowired
+	private ReportService reportService;
+	
+	@GetMapping
+	public List<GetAllReportsResponse> getAll(){
+		return this.reportService.getAll();
+	}
+	
+	@PostMapping
+	@ResponseStatus(code = HttpStatus.CREATED)
+	public void add(@RequestBody() CreateReportRequest createReportRequest) {
+		this.reportService.add(createReportRequest);
+	}
+	
+	@PutMapping
+	public void update(@RequestBody() UpdateReportRequest updateReportRequest) {
+		this.reportService.update(updateReportRequest);
+	}
+	
+	@DeleteMapping("/{id}")
+	public void delete(@PathVariable Long id) {
+		this.reportService.delete(id);
+	}
+	
+	@GetMapping("/{id}")
+	public GetReportResponse getReport(@PathVariable Long id) {
+		return this.reportService.getReport(id);
+	}
+	
 }
