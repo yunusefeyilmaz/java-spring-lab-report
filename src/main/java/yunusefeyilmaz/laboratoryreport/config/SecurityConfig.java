70e8ef3a5713130e4e96a9612686cc53f498415d
Yunus Efe Yılmaz <yunusefeyilmaz@outlook.com>
2024-05-24 18:10:45 +0300

    Add security config

---
+++ src/main/java/yunusefeyilmaz/laboratoryreport/config/SecurityConfig.java
@@ -0,0 +1,82 @@
+package yunusefeyilmaz.laboratoryreport.config;
+
+
+import java.util.Arrays;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+
+import lombok.AllArgsConstructor;
+import yunusefeyilmaz.laboratoryreport.business.concretes.LabAssistantDetailsService;
+import yunusefeyilmaz.laboratoryreport.security.JwtAuthenticationEntryPoint;
+import yunusefeyilmaz.laboratoryreport.security.JwtAuthenticationFilter;
+
+@EnableWebSecurity
+@AllArgsConstructor
+@Configuration
+public class SecurityConfig{
+	
+	private LabAssistantDetailsService labAssistantDetailsService;
+	private JwtAuthenticationEntryPoint handler;
+	
+	@Bean
+	public JwtAuthenticationFilter jwtAuthenticationFilter() {
+		return new JwtAuthenticationFilter();
+	}
+	
+	@Bean
+	public PasswordEncoder passwordEncoder() {
+		return new BCryptPasswordEncoder();
+	}
+	@Bean
+    public AuthenticationManager authenticationManager(AuthenticationConfiguration authenticationConfiguration) throws Exception {
+        return authenticationConfiguration.getAuthenticationManager();
+    }
+    
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http
+            .cors(cors -> cors.configurationSource(corsConfigurationSource()))
+            .csrf().disable()
+            .exceptionHandling(exceptionHandling -> 
+                exceptionHandling.authenticationEntryPoint(handler))
+            .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS).and()
+            .authorizeHttpRequests(authorizeRequests -> 
+                authorizeRequests
+	                .requestMatchers(HttpMethod.POST, "/api/auth/**").permitAll()
+	                .anyRequest().authenticated()
+            );
+        http.addFilterBefore(jwtAuthenticationFilter(), UsernamePasswordAuthenticationFilter.class);
+        return http.build();
+    }
+    
+    @Bean
+    public CorsConfigurationSource corsConfigurationSource() {
+        CorsConfiguration configuration = new CorsConfiguration();
+        configuration.setAllowCredentials(true);
+        configuration.setAllowedOrigins(Arrays.asList("*")); 
+        configuration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE", "OPTIONS","HEAD","PATCH"));
+        configuration.setAllowedHeaders(Arrays.asList("*"));
+
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", configuration);
+
+        return source;
+    }
+
+
+
+}
