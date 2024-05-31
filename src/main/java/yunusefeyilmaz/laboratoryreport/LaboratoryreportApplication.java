60159a80a5d6f030c514a998e70d92487b0d65c0
Yunus Efe Yılmaz <yunusefeyilmaz@outlook.com>
2024-05-31 17:51:03 +0300

    Refactor .env

--- src/main/java/yunusefeyilmaz/laboratoryreport/LaboratoryreportApplication.java
+++ src/main/java/yunusefeyilmaz/laboratoryreport/LaboratoryreportApplication.java
@@ -6,18 +6,11 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
-import io.github.cdimascio.dotenv.Dotenv;
-
-
 @SpringBootApplication
 @RestControllerAdvice
 public class LaboratoryreportApplication {
 
 	public static void main(String[] args) {
-		Dotenv dotenv = Dotenv.load();
-		System.setProperty("DB_URL", dotenv.get("DB_URL"));
-        System.setProperty("DB_USERNAME", dotenv.get("DB_USERNAME"));
-        System.setProperty("DB_PASSWORD", dotenv.get("DB_PASSWORD"));
 		SpringApplication.run(LaboratoryreportApplication.class, args);
 	}
 	
--- src/test/java/yunusefeyilmaz/laboratoryreport/LaboratoryreportApplicationTests.java
+++ src/test/java/yunusefeyilmaz/laboratoryreport/LaboratoryreportApplicationTests.java
@@ -2,8 +2,14 @@ package yunusefeyilmaz.laboratoryreport;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.TestPropertySource;
 
 @SpringBootTest
+@TestPropertySource(properties = {
+	    "DB_URL=jdbc:postgresql://localhost:5432/db_lab_report",
+	    "DB_USERNAME=postgres",
+	    "DB_PASSWORD=123456789"
+	})
 class LaboratoryreportApplicationTests {
 
 	@Test
